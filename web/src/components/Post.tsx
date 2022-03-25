692416405e042257f7460df418f01782757fcb19
alisalim17 <alisalimli1899@gmail.com>
2022-03-25 15:36:54 +0400

    better naming

--- web/src/components/Post.tsx
+++ web/src/components/Post.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { PostFragmentFragment } from '../generated/graphql';
 
-interface PostProps {}
+interface PostProps {
+  post: PostFragmentFragment;
+}
 
 const Page: React.FC<PostProps> = () => {
   return <div>hello world</div>;
