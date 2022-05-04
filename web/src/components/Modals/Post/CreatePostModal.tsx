97591dfa7e2519200774575fc9caca6655365beb
alisalim17 <alisalimli1899@gmail.com>
2022-05-04 11:24:46 +0400

    form classes in css & react-select

--- web/src/components/Modals/Post/CreatePostModal.tsx
+++ web/src/components/Modals/Post/CreatePostModal.tsx
@@ -1,17 +1,33 @@
 import Modal from '@/ui/Modal/Modal';
 import { Form, Formik } from 'formik';
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@/ui/buttons/Button';
 import Divider from '@/ui/Divider';
-import InputField from '@/ui/InputField';
+import InputField from '@/ui/Form/InputField';
+import Creatable from 'react-select/creatable';
+import { useTagsQuery } from '@/generated/graphql';
+import Label from '../../../ui/Form/Label';
 
 const CreatePostModal = () => {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
+  const [{ data }] = useTagsQuery();
+  const arr = [];
+  data?.tags?.map((i) =>
+    arr.push({
+      label: i.name,
+      value: i.name,
+    })
+  );
   return (
     <Modal openModalText='+ Create a new one'>
       <Modal.Header>Create post</Modal.Header>
       <Modal.Body>
         <Formik
-          initialValues={{ title: '', tags: '', url: '', category: '' }}
+          initialValues={{ title: '', tags: [], url: '', category: '' }}
           onSubmit={async (props) => {
             console.log('props', props);
             //   router.push({
@@ -20,7 +36,7 @@ const CreatePostModal = () => {
             //   });
           }}
         >
-          {() => (
+          {({ setFieldValue }) => (
             <Form className='mt-12 space-y-4'>
               <InputField
                 name='title'
@@ -32,11 +48,15 @@ const CreatePostModal = () => {
                 label='Url'
                 placeholder='Enter youtube video url...'
               />
-              <InputField
-                name='tags'
-                label='Tags'
-                placeholder='Enter video tag(s)...'
-              />
+              <div>
+                <Label fieldName='tags'>Tags</Label>
+                <Creatable
+                  name='tags'
+                  isMulti
+                  onChange={(tagsArr) => setFieldValue('tags', tagsArr)}
+                  options={arr}
+                />
+              </div>
               <InputField
                 name='category'
                 label='Category'
