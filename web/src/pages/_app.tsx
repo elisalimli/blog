0788b3651ca3761d7b9c7a34745798bb5decb6e6
alisalim17 <alisalimli1899@gmail.com>
2022-05-14 13:14:23 +0400

    fix full width in small device

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,24 +1,27 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { withUrqlClient } from 'next-urql';
+import NextNProgress from 'nextjs-progressbar';
+import React from 'react';
 
 import '@/styles/globals.css';
 
-import LayoutWrapper from '@/components/LayoutWrapper';
-import NextNProgress from 'nextjs-progressbar';
-import { createUrqlClient } from '../utils/createUrqlClient';
-import { withUrqlClient } from 'next-urql';
+import { createUrqlClient } from '@/utils/createUrqlClient';
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <meta content='width=device-width, initial-scale=1' name='viewport' />
+        <meta
+          content='width=device-width,initial-scale=1,user-scalable=no,user-scalable=0'
+          name='viewport'
+        />
       </Head>
       <NextNProgress />
-
+      {/* @ts-ignore */}
       <Component {...pageProps} />
     </>
   );
-}
+};
 
 export default withUrqlClient(createUrqlClient)(MyApp);
