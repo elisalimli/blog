df94e3820fc94cf91a51f07cf970e63a8791a8e7
alisalim17 <alisalimli1899@gmail.com>
2022-05-25 00:01:33 +0400

    making category slide responsive

--- web/src/components/Categories/Categories.tsx
+++ web/src/components/Categories/Categories.tsx
@@ -19,14 +19,36 @@ const Categories: React.FC = () => {
     <>
       {/* @todo complete this */}
       <Swiper
-        slidesPerView={4}
         spaceBetween={50}
         pagination={{ dynamicBullets: true, clickable: true }}
         modules={[Pagination]}
+        autoHeight
+        breakpoints={{
+          // when window width is >= 320px
+          320: {
+            slidesPerView: 1,
+            autoHeight: true,
+            spaceBetween: 20,
+          },
+          // when window width is >= 480px
+          480: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          // when window width is >= 640px
+          640: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+          },
+        }}
         className='select-none'
       >
         {data?.categories?.map(({ name, id }) => (
-          <SwiperSlide style={{ height: 500 }} key={`category-${id}`}>
+          <SwiperSlide key={`category-${id}`}>
             <UnstyledLink href={`/search?query=${name}`}>
               <div className='relative'>
                 <div
