46ec2ee27c5a09b3656db27fb6ad1d7796594034
alisalim17 <alisalimli1899@gmail.com>
2022-06-12 13:58:39 +0400

    uploadImage function

--- server/src/graphql/resolvers/category/createCategory/createCategory.ts
+++ server/src/graphql/resolvers/category/createCategory/createCategory.ts
@@ -1,10 +1,7 @@
-import sharp from "sharp";
 import { Arg, Ctx, Mutation, Resolver, UseMiddleware } from "type-graphql";
-import { v4 as uuidv4 } from "uuid";
 import { Category } from "../../../../../generated";
-import { FILE_EXTENSION } from "../../../../utils/constants";
 import { MyContext } from "../../../../utils/MyContext";
-import { streamToBuffer } from "../../../../utils/streamToBuffer";
+import { uploadImage } from "../../../../utils/uploadImage";
 import { CreateCategoryInput } from "../../../inputs/category/CreateCategoryInput";
 import { isAuth } from "../../../middlewares/isAuth";
 import { isAuthor } from "../../../middlewares/isAuthor";
@@ -18,21 +15,7 @@ export class CreateCategoryResolver {
     @Ctx() { prisma }: MyContext,
     @Arg("input") { file, name }: CreateCategoryInput
   ): Promise<boolean> {
-    console.log("file", file);
-    const { createReadStream, filename } = await file;
-    const fileNameWithoutExtension = filename.split(".")[0];
-    const file_name = `${uuidv4()}-${fileNameWithoutExtension}`;
-
-    const imagePath =
-      __dirname + `/../../../../../images/${file_name}.${FILE_EXTENSION}`;
-
-    const image = await streamToBuffer(createReadStream());
-
-    const a = await sharp(image as any)
-      .jpeg({ quality: 20 })
-      .resize(600, 600)
-      .toFile(imagePath);
-    console.log(a);
+    const file_name = await uploadImage(file);
 
     await prisma.category.create({ data: { name, pictureUrl: file_name } });
     return true;
---
+++ server/src/utils/uploadImage.ts
@@ -0,0 +1,22 @@
+import { FileUpload } from "graphql-upload";
+import sharp from "sharp";
+import { FILE_EXTENSION } from "./constants";
+import { streamToBuffer } from "./streamToBuffer";
+import { v4 as uuidv4 } from "uuid";
+
+export const uploadImage = async (file: FileUpload) => {
+  const { createReadStream, filename } = await file;
+  const fileNameWithoutExtension = filename.split(".")[0];
+  const file_name = `${uuidv4()}-${fileNameWithoutExtension}`;
+
+  const imagePath =
+    __dirname + `/../../../../../images/${file_name}.${FILE_EXTENSION}`;
+
+  const image = await streamToBuffer(createReadStream());
+
+  await sharp(image as any)
+    .jpeg({ quality: 20 })
+    .resize(600, 600)
+    .toFile(imagePath);
+  return file_name;
+};
